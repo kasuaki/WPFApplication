ffa70de5393ceceffcec8ed93332c834de0833d0
kasuaki <k.kawa@d5.dion.ne.jp>
2014-09-12 18:27:06 +0900

    追加漏れ

---
+++ MvvmLight1/MyWebBrowser.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MvvmLight1
+{
+    /// <summary>
+    /// Description for MyWebBrowser.
+    /// </summary>
+    public partial class MyWebBrowser : Grid
+    {
+        /// <summary>
+        /// Initializes a new instance of the MyWebBrowser class.
+        /// </summary>
+        public MyWebBrowser()
+        {
+            InitializeComponent();
+        }
+    }
+}
